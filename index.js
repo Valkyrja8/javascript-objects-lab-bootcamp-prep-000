3f12504d2b83030564bbb9c50c8d53ebcef54a1f
Anais Trope <trope.anais@gmail.com>
2019-04-30 13:32:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var recipes={}
 
 function updateObjectWithKeyAndValue(object, key, value){
+<<<<<<< HEAD
  var newObj = Object.assign({}, object,{[key]: value})
  return newObj;
+=======
+ return Object.assign({}, object,{[key]: value});
+>>>>>>> 47fa6051990b2638baa7b00ad2f6db8697d60464
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
+<<<<<<< HEAD
 }
 
 function deleteFromObjectByKey(newObj, key){
@@ -16,4 +21,6 @@ return delete newObj.key
   
 function destructivelyDeleteFromObjectByKey(object, key){
  return delete object[key]
+=======
+>>>>>>> 47fa6051990b2638baa7b00ad2f6db8697d60464
 }
